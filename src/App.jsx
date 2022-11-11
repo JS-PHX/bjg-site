acadfc6f623de78c461086eaa43ec27123b4efcc
Jesus Sanchez <jsanchez@Jesuss-MacBook-Pro.local>
2022-11-10 18:21:14 -0700

    11/10 update

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+import Nav from "./components/nav"
+
+export default function App(){
+    return (
+        <div>
+            <Nav />
+            <h1>hello</h1>
+        </div>
+    )
+}
---
+++ src/components/nav.jsx
@@ -0,0 +1,9 @@
+export default function Nav(){
+    return (
+        <nav className="navbar">
+            <h3><a href="index.html">Home</a></h3>
+            <h3><a href="index.html">Contact</a></h3>
+            <h3><a href="index.html">Gallery</a></h3>
+        </nav>
+    )
+}
