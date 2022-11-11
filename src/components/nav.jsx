812c76a709df510341d30610bb8721f2ce604b8e
Jesus Sanchez <jsanchez@Jesuss-MacBook-Pro.local>
2022-11-10 19:57:00 -0700

    pics

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,6 +1,7 @@
+
 export default function Nav(){
     return (
-        <nav className="navbar">
+        <nav className='navbar'>
             <h3><a href="index.html">Home</a></h3>
             <h3><a href="index.html">Contact</a></h3>
             <h3><a href="index.html">Gallery</a></h3>
