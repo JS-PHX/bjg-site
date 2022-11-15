7f9f7f6a70dacc6e375c1eb6fdaf027f578d4172
Jesus Sanchez <jsanchez@Jesuss-MacBook-Pro.local>
2022-11-14 17:44:21 -0700

    uodate

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,9 +3,9 @@ export default function Nav(){
         <header>
         <nav className="navbar">
             <h1 >BJG Trucking</h1>
-            <h3><ul><a href="index.html">Home</a></ul></h3>
-            <h3><ul><a href="index.html">Contact</a></ul></h3>
-            <h3><ul><a href="index.html">Gallery</a></ul></h3>
+            <h3><a href="index.html">Home</a></h3>
+            <h3><a href="index.html">Contact</a></h3>
+            <h3><a href="index.html">Gallery</a></h3>
         </nav>
         </header>
     )
