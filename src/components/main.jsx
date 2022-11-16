12ea4e1cf09dfb411b02cbca0d07808257be0ccf
Jesus Sanchez <jsanchez@Jesuss-MacBook-Pro.local>
2022-11-15 18:33:18 -0700

    updates

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,9 +1,11 @@
 export default function Home(){
     return (
-        <content>
-           <body className="home-page">
-            <h1>Welcome</h1>
-           </body>
-        </content>
+        <body>
+           <container className='homepage'>
+            <p className="about">BJG Trucking, Inc. is family owned business founded in 2003 by sister company BJG Grading, Inc. <br />
+            We bring our customers with over 15 years of experience in the industry. Keeping quality and competitive prices a priority. <br />
+		    Our plant accounts through out the valley and a fleet of dump trucks allow us to arrive at job site in a timely manner and keep costs down. We are here to serve your trucking needs.</p>
+           </container>
+        </body>
     )
 }
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,10 +2,10 @@ export default function Nav(){
     return (
         <header>
         <nav className="navbar">
-            <h1 >BJG Trucking</h1>
-            <h3><a href="index.html">Home</a></h3>
-            <h3><a href="index.html">Contact</a></h3>
             <h3><a href="index.html">Gallery</a></h3>
+            <h3><a href="index.html">Contact</a></h3>
+            <h3><a href="index.html">Home</a></h3>
+            <h1 className="title">BJG Trucking</h1>
         </nav>
         </header>
     )
