17be4d2b3986bfe3b1e9f7a8bbed511b8fb6a7e3
Jesus Sanchez <jsanchez@Jesuss-MacBook-Pro.local>
2022-11-15 19:22:32 -0700

    uodates

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import Nav from "./components/nav"
 import Home from "./components/main"
+import Phone from "./components/contact" 
 
 export default function App(){
     return (
---
+++ src/components/contact.jsx
@@ -0,0 +1,10 @@
+export default function Phone(){
+    return (
+        <div>
+            <content className="number">
+                <h3>4345</h3>
+
+            </content>
+        </div>
+    )
+}
