849b90b2c02a09a3a7860996afb6b0a6492320c5
Jesus Sanchez <jsanchez@Jesuss-MacBook-Pro.local>
2022-11-17 18:20:45 -0700

    logo

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,11 +2,11 @@ export default function Nav(){
     return (
         <header>
         <nav className="navbar">
+            <img className="logo" src="./pics/logo2.png" alt="logo"></img>
             <h3><a href="index.html">Gallery</a></h3>
             <h3><a href="index.html">Contact</a></h3>
             <h3><a href="index.html">Home</a></h3>
             {/* <h1 className="title">BJG Trucking</h1> */}
-            <img src="bjglogo.jpg" alt="logo" height="25px" width="35px"></img>
         </nav>
         </header>
     )
