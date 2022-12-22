13c56db0b1b825341fdb6b29808adfd650185a10
Jesus Sanchez <jsanchez@Jesuss-MacBook-Pro.local>
2022-12-22 16:57:27 -0700

    email

--- src/components/EmailForm.jsx
+++ src/components/EmailForm.jsx
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import emailjs from 'emailjs-com';
+import emailjs from "@emailjs/browser";
 
 export const ContactUs = () => {
   const form = useRef();
@@ -7,7 +7,12 @@ export const ContactUs = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('gmail', 'template_i24q4zp', form.current, 'WHpfLyqJPl10QF4qF')
+    emailjs.sendForm(
+      'service_mscgxyq',
+      'template_i24q4zp',
+      form.current,
+      'WHpfLyqJPl10QF4qF'
+        )
       .then((result) => {
           console.log(result.text);
       }, (error) => {
@@ -19,7 +24,7 @@ export const ContactUs = () => {
   return (
     <div className='message'>
     <form ref={form} onSubmit={sendEmail}>
-      <h1 className='contactTitle'>Contact</h1>
+      <h1 className='contactTitle'>Contact Us</h1>
       <input placeholder="Name" type="text" name="user_name" />
       <input placeholder="E-Mail" type="email" name="user_email" /><br /><br />
       <textarea placeholder="Message" name="message" />
