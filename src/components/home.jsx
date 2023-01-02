f8cde27bfad86376a01ab7ed31e90816d1e85f9b
Jesus Sanchez <jsanchez@Jesuss-MacBook-Pro.local>
2023-01-02 15:32:01 -0700

    gallery

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,19 +2,20 @@
 export default function Home(){
     return (
         <body>
-           <container className='homepage'>
-                <p className="about">BJG Trucking, Inc. is family owned business founded in 2003 by sister company BJG Grading, Inc. <br />
-                We bring our customers with over 15 years of experience in the industry. Keeping quality and competitive prices a priority. <br />
-                Our plant accounts through out the valley and a fleet of dump trucks allow us to arrive at job site in a timely manner and keep costs down. We are here to serve your trucking needs.</p>
+            {/* <h1 className="greeting">Welcome</h1> */}
+           <div className='homepage'>
+                <p className="about" data-hover="BJG Trucking, Inc. is family owned business founded in 2003 by sister company BJG Grading, Inc.
+                We bring our customers with over 15 years of experience in the industry. Keeping quality and competitive prices a priority.
+                Our plant accounts through out the valley and a fleet of dump trucks allow us to arrive at job site in a timely manner and keep costs down. We are here to serve your trucking needs.">About Us</p>
            <div className="number">
                 <h3 className="phonenum">
                 Contact <br />
-                Phone: (602) 200-7028
+                Phone: (602) 200-7028<br />
                 Fax: (602) 200-7029
                 </h3>
 
             </div>
-           </container>
+           </div>
         </body>
     )
 }
