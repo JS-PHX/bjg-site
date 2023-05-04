fd215e59f58f279ebfeee2bc2f42b6d39c2c384d
Jesus Sanchez <jsanchez@Jesuss-MacBook-Pro.local>
2023-05-03 20:10:54 -0700

    updates

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -15,24 +15,28 @@ export default function Home(){
                 We bring our customers with over 15 years of experience in the industry. Keeping quality and competitive prices a priority.
                 Our plant accounts through out the valley and a fleet of dump trucks allow us to arrive at job site in a timely manner and keep costs down. We are here to serve your trucking needs.">About Us</p>} */}
         <section class="container">
-            <h2>Welcome to BJG Trucking</h2>
+        <div class="text-container">
+            <p>
+                <span class="underline-hover">Welcome to BJG Trucking</span>
+            </p>
+        </div>
             <div class="text-container reveal fade-left">
-                <div class="text-box" class="firstBox">
+                <div  class="firstBox">
                     <h3>BJG Trucking Inc. is a family owned business founded in 2003 by sister company BJG Grading Inc.</h3>
                 </div>
             </div>
             <div class="text-container reveal fade-left">
-                <div class="text-box" class="secondBox">
+                <div class="secondBox">
                     <h3>We bring our customers over 15 years of experience in the industry, keeping quality and competitive prices a priority.</h3>
                 </div>
             </div>
             <div class="text-container reveal fade-left">
-                <div class="text-box" class="thirdBox">
+                <div  class="thirdBox">
                     <h3>Our plant accounts throughout the valley, and a fleet of dump trucks allow us to arrive at job site in a timely manner, and keep costs down. We are here to serve your trucking needs.</h3>
                 </div>
             </div>
             <div class="text-container reveal fade-bottom">
-                <div class="text-box" class="contactBox" style={{float:"right", marginLeft:"5rem", width:"auto"}}>
+                <div class="contactBox" style={{float:"right", marginLeft:"5rem", width:"auto"}}>
                     <h3>Contact</h3>
                     <h3>Phone: (602) 200-7028</h3>
                     <h3>Fax: (602) 200-7029</h3>
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -4,9 +4,9 @@ export default function Nav(){
     return (
         <header>
         <nav class="navbar">
-                <p><Link class="navLinks" to="/gallery">Gallery</Link></p>
+                <p><Link class="navLinks" to="/home">Home</Link></p>
                 <p><Link class="navLinks" to="/EmailForm">Contact</Link></p>
-                <p><Link class="navLinks" to="/">Home</Link></p>
+                <p><Link class="navLinks" to="/gallery">Gallery</Link></p>
             <Link to="/">
                 <img class="logoLink" src="./pics/TruckLogo.png" alt="logo"></img>
             </Link>
