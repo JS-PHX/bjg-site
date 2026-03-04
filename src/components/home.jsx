e89a84b01c18c67d3af1f70d0e601669a630b66e
Jesus Sanchez <jsanchez@Jesuss-MacBook-Pro.local>
2026-03-04 15:52:04 -0700

    reveal removal

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -26,13 +26,13 @@ export default function Home(){
       With a strong foundation in the construction and hauling industry, we proudly serve the Greater Phoenix area and surrounding regions.</h3>
     </div>
   </div>
-  <div class="text-container2 reveal">
+  <div class="text-container2">
     <div class="secondBox">
       <h3>With over 15 years of professional experience, BJG Trucking Inc. is committed to providing dependable, high-quality trucking services. 
       We prioritize both service excellence and competitive pricing to meet the demands of today’s commercial and industrial job sites.</h3>
     </div>
   </div>
-  <div class="text-container3 reveal">
+  <div class="text-container3">
     <div class="thirdBox">
       <h3>Our well-maintained fleet of dump trucks and strong network of plant accounts throughout the Valley enable us to deliver materials efficiently and on time. 
       Whether it’s bulk hauling, construction material transport, or custom trucking needs, we are equipped to support projects of any scale.</h3>
