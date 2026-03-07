f25a6f5ebf6bcd7ccc95117a2f87dba92bffaeb5
Jesus Sanchez <jsanchez@Jesuss-MacBook-Pro.local>
2026-03-06 17:00:28 -0700

    undo

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,25 +1,37 @@
 import { Link } from "react-router-dom";
 
 export default function Nav() {
-  return (
-    <header>
-      <nav className="navbar">
-        <div className="navbar-logo">
-          <Link to="/">
-            <img
-              className="logoLink"
-              src="./pics/TruckLogo.png"
-              alt="logo"
-            />
-          </Link>
-        </div>
-
-        <div className="navbar-links">
-          <Link className="navLinks" to="/home">Home</Link>
-          <Link className="navLinks" to="/EmailForm">Contact</Link>
-          <Link className="navLinks" to="/gallery">Gallery</Link>
-        </div>
-      </nav>
-    </header>
-  );
-}
+    return (
+      <header>
+        <nav className="navbar">
+          <div className="navbar-links">
+            <p>
+              <Link className="navLinks" to="/home">
+                Home
+              </Link>
+            </p>
+            <p>
+              <Link className="navLinks" to="/gallery">
+                Gallery
+              </Link>
+            </p>
+            <p>
+              <Link className="navLinks" to="/EmailForm">
+                Contact
+              </Link>
+            </p>
+          </div>
+          <div className="navbar-logo">
+            <Link to="/">
+              <img
+                className="logoLink"
+                src="./pics/TruckLogo.png"
+                alt="logo"
+              ></img>
+            </Link>
+          </div>
+        </nav>
+      </header>
+    );
+  }
+  
